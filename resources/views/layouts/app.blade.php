df913a0cc0cf30f0ab7722b4d4646ff2dc4a807b
Adam Williams <adam@whitesunrise.com>
2016-08-12 14:08:57 +0000

    Hello

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,9 +46,11 @@
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
-                <!-- <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>-->
+                @if (!Auth::guest())
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/upload') }}">Upload</a></li>
+                </ul>
+                @endif
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
